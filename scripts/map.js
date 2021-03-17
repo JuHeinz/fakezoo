c2f0b1887f85f9f99078f4aa70d5e1e42dfe5490
fakezoo <66963403+fakezoo@users.noreply.github.com>
2021-03-17 13:25:18 +0100

    k

--- scripts/map.js
+++ scripts/map.js
@@ -1,24 +1,45 @@
 console.log("map.js connected!");
-var mapcell = document.querySelectorAll(".mapcell");
-
+var mapcells = document.querySelectorAll(".mapcell");
+const cellHolder = document.querySelector("#cellHolder")
 // Eventlistener all cells
-// wenn man bei vielen elementen der selben klasse einen eventlistener hinzufügen will braucht man einen for loop
-for (var i = 0; i < mapcell.length; i++){
-    mapcell[i].addEventListener("click", showAnimal);
-}
-
-function showAnimal() {
-console.log("I clicked on a map cell.");
+for (let cell of mapcells){
+    cell.addEventListener("click",displayInfo)
 }
 
 
-// Just one specific cell
-var lo = document.querySelector("#lo");
-lo.addEventListener("mouseover", test)
+function displayInfo(){
+    this.style.border ="3px solid black"
+    /// Für jeder Mapcell soll ein tooltip mit dem tiernamen gezeigt werden.
+    // create Tooltip in HTML
+}
 
-function test() {
-    
-    lo.classList.add("test")
+// functionen via keyboard commands aktivieren
+// switch macht eigentlich nur sinn wenn es mehrere wären
+window.addEventListener("keydown", event =>{
+    switch(event.key){
+        case "e":
+        expandZoo()
+        break;
     }
+}
+)
 
 
+// diese Function fügt eine neue Mapcell hinzu
+function expandZoo(){
+    const newCell = document.createElement("div")
+    newCell.classList.add("mapcell", "col-sm-4")
+    const newImg = document.createElement("img")
+    newImg.setAttribute("src", "./images/placeholderanimal.png")
+    newImg.classList.add("img-fluid")
+    newCell.append(newImg)
+    cellHolder.append(newCell);
+}
+
+// Problem: Der oben definierte EventListener bezieht sich nicht auf neu erstellte Objekte, deshalb:
+cellHolder.addEventListener("click",function(event){
+    console.dir(event.target)
+    if (event.target.className === "img-fluid"){
+        event.target.style.border ="3px solid red"
+    }
+})
